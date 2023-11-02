64ceca0566fd1be79168f067d587b5af11c3767c
NinaMolinari <ninamolinari@yahoo.com>
2023-11-02 13:56:21 -0700

    corrections

--- extension/popup.js
+++ extension/popup.js
@@ -1,10 +1,44 @@
-document.getElementById('listClusters').addEventListener('click', function() {
-    fetch('http://localhost:8080/list-clusters')
-    .then(response => response.text())
+// document.getElementById('listClusters').addEventListener('click', function() {
+//     fetch('http://localhost:8080/list-clusters')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('output').innerText = data;
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// });
+
+let token = null;
+
+function login() {
+    fetch('http://localhost:8080/login', {
+        method: 'POST'
+    })
+    .then(response => response.json())
     .then(data => {
-        document.getElementById('output').innerText = data;
+        token = data.token;
+    })
+    .catch(error => {
+        console.error('Login error:', error);
+    });
+}
+
+document.getElementById('listClusters').addEventListener('click', function() {
+    fetch('http://localhost:8080/list-clusters', {
+        headers: {
+            'Authorization': token
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const clusters = data.clusters.join(", ");
+        document.getElementById('output').innerText = "Clusters: " + clusters;
     })
     .catch(error => {
         console.error('Error:', error);
     });
 });
+
+login();
+
